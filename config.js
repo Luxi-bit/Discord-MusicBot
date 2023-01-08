23782bbf7789dcd532973036fc218978538f2e25
Rivaldo Lietianto <liealdo0@gmail.com>
2023-01-08 08:43:54 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,14 @@
 module.exports = {
   cmdPerPage: 10, //- Number of commands per page of help command
-  adminId: "488904563944849429", //- Replace UserId with the Discord ID of the admin of the bot
-  token:
-    process.env.token ||
-    "OTc4OTU0Njc0MjY4OTM0MTY0.G49IHN.y6Gzy8Dg7HSQp5kR6xPUYr6oM99e4i9YZOGU-g", //- Bot's Token
-  clientId: process.env.clientId || "978954674268934164", //- ID of the bot
-  clientSecret: process.env.clientSecret || "MxierGx1_Zv3tyxBa7RhmGJ6GpBjjWPc", //- Client Secret of the bot
+  adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
+  token: process.env.token || "", //- Bot's Token
+  clientId: process.env.clientId || "", //- ID of the bot
+  clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
   port: 4200, //- Port of the API and Dashboard
   scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
   serverDeafen: true, //- If you want bot to stay deafened
   defaultVolume: 100, //- Sets the default volume of the bot, You can change this number anywhere from 1 to 100
-  supportServer: "https://discord.gg/rKZb9XjGpK", //- Support Server Link
+  supportServer: "https://discord.gg/sbySMS7m3v", //- Support Server Link
   Issues: "https://github.com/SudhanPlayz/Discord-MusicBot/issues", //- Bug Report Link
   permissions: 277083450689, //- Bot Inviting Permissions
   disconnectTime: 30000, //- How long should the bot wait before disconnecting from the voice channel (in miliseconds). Set to 1 for instant disconnect.
@@ -18,22 +16,22 @@ module.exports = {
   autoQueue: false, //- When set to true, related songs will automatically be added to the queue
   autoPause: true, //- When set to true, music will automatically be paused if everyone leaves the voice channel
   debug: false, //- Debug mode
-  cookieSecret: "Chengs is Amazing", //- Cookie Secret
+  cookieSecret: "CodingWithSudhan is epic", //- Cookie Secret
   website: "http://localhost:4200", //- without the / at the end
   // You need a lavalink server for this bot to work!!!!
   // Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
   nodes: [
     {
       identifier: "Main Node", //- Used for indentifier in stats commands.
-      host: "nonssl.freelavalink.ga", //- The host name or IP of the lavalink server.
+      host: "", //- The host name or IP of the lavalink server.
       port: 80, // The port that lavalink is listening to. This must be a number!
-      password: "www.freelavalink.ga", //- The password of the lavalink server.
+      password: "", //- The password of the lavalink server.
       retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
       retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
       secure: false, //- Can be either true or false. Only use true if ssl is enabled!
     },
   ],
-  embedColor: "#1cfc03", //- Color of the embeds, hex supported
+  embedColor: "#2f3136", //- Color of the embeds, hex supported
   presence: {
     // PresenceData object | https://discord.js.org/#/docs/main/stable/typedef/PresenceData
     status: "online", //- You can have online, idle, dnd and invisible (Note: invisible makes people think the bot is offline)
